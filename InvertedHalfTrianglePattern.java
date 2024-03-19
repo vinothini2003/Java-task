b4bf304905b44048b44b689c9db4a8424a167d84
vinothini2003 <128352926+vinothini2003@users.noreply.github.com>
2024-03-19 07:43:30 +0530

    Create InvertedHalfTrianglePattern.java

---
+++ InvertedHalfTrianglePattern.java
@@ -0,0 +1,19 @@
+package PatternProgram;
+
+public class InvertedHalfTrianglePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row = 5;
+		for(int i=0; i<=row; i++){
+		for(int j=i; j<=row;j++){
+		System.out.print("*");
+		}
+		System.out.println();
+		}
+		
+
+	}
+
+}
+
