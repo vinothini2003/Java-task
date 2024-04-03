74c674ad460973364815d8b68264e5419fcb702c
vinothini2003 <128352926+vinothini2003@users.noreply.github.com>
2024-04-03 19:36:34 +0530

    Create vino.java

---
+++ vino.java
@@ -0,0 +1,14 @@
+public class vino{
+  public static void main(String[] args){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter your number");
+    num = sc.nextInt;
+
+    if(num %2==0){
+      System.out.println("The given number is even");
+    }
+    else{
+      System.out.println("The given number is odd");
+    } 
+  } 
+}
